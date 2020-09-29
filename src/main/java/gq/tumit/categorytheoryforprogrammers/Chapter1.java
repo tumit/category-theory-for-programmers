cbf8b0cc6c6476c968ad84965ba38013cbcb93c5
tumit <tumit@live.com>
2020-09-29 15:12:46 +0700

    test pre-commit again

--- src/main/java/gq/tumit/categorytheoryforprogrammers/Chapter1.java
+++ src/main/java/gq/tumit/categorytheoryforprogrammers/Chapter1.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 public class Chapter1 {
 
-    public static <T> T identity(T x) {
-        return x;
-    }
+  public static <T> T identity(T x) {
+    return x;
+  }
 
-    public static <T> Function<T, T> composite(Function<T, T> fn1, Function<T, T> fn2) {
-        return fn1.compose(fn2);
-    }
+  public static <T> Function<T, T> composite(Function<T, T> fn1, Function<T, T> fn2) {
+    return fn1.compose(fn2);
+  }
 }
